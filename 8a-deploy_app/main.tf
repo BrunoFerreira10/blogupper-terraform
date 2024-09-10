6da3298fffa7786366da2aadf3f5b0f434512e1b
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-09-10 10:57:15 -0300

    Correcao nomes build e deploy

--- 8a-deploy_app/main.tf
+++ 8a-deploy_app/main.tf
@@ -2,11 +2,11 @@ module "deploy_app" {
   source                   = "../modules/developer/tools/generic_code_deploy"
   app_repository_url_https = module.data.github_vars.app_repository_url_https
   codebuild_settings = { # TODO - Rever essa divisão
-    project_name = "EasyAppointments"
+    project_name = module.data.github_vars.general_tag_shortname
     github_connection_name = module.data.github_vars.my_github_connection_name
   }
   codedeploy_settings = { # TODO - Rever essa divisão
-    application_name = "EasyAppointments"
+    application_name = module.data.github_vars.general_tag_shortname
     target_group = module.data.projects.elb_app.target_group
     elb = module.data.projects.elb_app.elb
     asg = module.data.projects.elb_app.asg
